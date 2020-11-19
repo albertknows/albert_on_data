74417ca78a4a39bf16e558c51ed7910f07bdd23f
albertknows <71308782+albertknows@users.noreply.github.com>
2020-11-19 02:12:58 -0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     <div class="home-container" >
       <div class="u-full-width">
         <h1 class="site-title">
-            greg<span class="txt-color"> on </span>data
+            albert<span class="txt-color"> </span>farrales
         </h1>
         <h5 class="subtitle" id="type-it-text">
           <!-- Fancy Typed Text -->
