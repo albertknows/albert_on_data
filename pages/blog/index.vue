4117b059c5e66d55023cbc40de2ed22ff423b2bb
albertknows <71308782+albertknows@users.noreply.github.com>
2020-12-07 23:12:47 -0800

    Update index.vue

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -5,7 +5,6 @@
     <div v-for="article of articles" :key="article.slug">
         <h3 class="mb10">
           <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">
-              {{ article.title.toLowerCase() }}
           </NuxtLink>
         </h3>
         <p><em>{{ (article.date || article.createdAt) | moment("MMMM Do, YYYY") }}</em></p>
