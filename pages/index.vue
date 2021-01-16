c710a4a7bffe2d62ba9d42d4b4196afb47b8785f
albertknows <71308782+albertknows@users.noreply.github.com>
2021-01-16 13:51:22 -0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     <div class="home-container" >
       <div class="u-full-width">
         <h1 class="site-title">
-            [insert ti<span class="txt-color">tle here]</span>
+            [insert<span class="txt-color"> title]</span>
         </h1>
         <h5 class="subtitle" id="type-it-text">
           <!-- Fancy Typed Text -->
